f2be3a49db1fb704debb6695a84f5a70616b5721
alivar391 <patriot391@mail.ru>
2021-08-10 20:58:04 +0300

    feat:modified index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+        if (array == undefined || array.length==0 ) return 0;
+    else {
+        let arr = array.sort((a, b)=> a - b);
+        return arr[0];
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    
+    if (array == undefined || array.length==0 ) return 0;
+    else {
+        let arr = array.sort((a, b)=> a - b);
+        return arr[arr.length-1];
+    }
+    
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    
+    if (array == undefined || array.length==0 ) return 0;
+    else {
+        let result = array.reduce((sum, current) => sum + current, 0) / array.length;
+        return result;
+    }
 }
